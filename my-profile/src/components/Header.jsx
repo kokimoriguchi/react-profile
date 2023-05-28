4894db908b5a16ecb109a1b81b96b7b653a06eb8
kokimoriguchi <skoki5231@icloud.com>
2023-05-28 21:55:32 +0900

    deploy

--- my-profile/src/components/Header.jsx
+++ my-profile/src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Link, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import Contact from "./Contact";
 import ProfileDetail from "./ProfileDetail";
@@ -6,7 +6,7 @@ import Portfolio from "./Portfolio";
 
 const Header = () => {
   return (
-    <BrowserRouter>
+    <Router>
       <header className="bg-gray-800 text-white sticky top-0 z-50">
         <nav className="flex items-center justify-between container mx-auto py-4">
           <div className="text-xl font-bold">MY PROFILE</div>
@@ -33,7 +33,7 @@ const Header = () => {
         <Route path="/Portfolio" element={<Portfolio />} />
         <Route path="/Contact" element={<Contact />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 };
 export default Header;
